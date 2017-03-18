7c73b32fb7efd40719f1192826fe51b0fdf7c5c0
RyanN <r.nagle64@gmail.com>
2017-03-18 12:15:42 -0700

    weekly work

--- src/Scene.java
+++ src/Scene.java
@@ -3,83 +3,101 @@ import java.util.List;
 
 public class Scene {
 	private List<Object> sceneObjects = new ArrayList<Object>();
-	
-	public void addObject(Object o){
+
+	public void addObject(Object o) {
 		sceneObjects.add(o);
 	}
-	
-	public void update(){
+
+	public void update() {
 		updateObject(sceneObjects.size() - 1);
 	}
-	
-	/**	Recursively updates all objects in the scene
-	 *  @param object element to start updating from 
-	 *  Precondition: -1 <= obj < sceneObjects.size()
+
+	/**
+	 * Recursively updates all objects in the scene
+	 * 
+	 * @param object
+	 *            element to start updating from Precondition: -1 <= obj <
+	 *            sceneObjects.size()
 	 */
-	public void updateObject(int obj){
-		/* your code here */
+	public void updateObject(int obj) {
+		if (obj >= 0) {
+			sceneObjects.get(obj).update();
+			updateObject(obj - 1);
+		}
 	}
 }
 
 class Object {
 	private List<Component> attachedComponents = new ArrayList<Component>();
-	
-	public void addComponent(Component c){
+
+	public void addComponent(Component c) {
 		attachedComponents.add(c);
 	}
-	
-	public void update(){
+
+	public void update() {
 		updateComponent(attachedComponents.size() - 1);
 	}
-	
-	/**	Recursively updates all components attached to the object
-	 *  @param component element to start updating from 
-	 *  Precondition: -1 <= com < sceneObjects.size()
+
+	/**
+	 * Recursively updates all components attached to the object
+	 * 
+	 * @param component
+	 *            element to start updating from Precondition: -1 <= com <
+	 *            sceneObjects.size()
 	 */
-	public void updateComponent(int com){
-		/* your code here */
+	public void updateComponent(int com) {
+		if (com >= 0) {
+			attachedComponents.get(com).update();
+			updateComponent(com - 1);
+		}
 	}
 }
 
 class Component {
 	private static int count = 0;
 	private int id;
-	
+
 	private boolean updated = false;
-	
+
 	private List<Component> attachedComponents = new ArrayList<Component>();
-	
-	public void addComponent(Component c){
+
+	public void addComponent(Component c) {
 		attachedComponents.add(c);
 	}
-	
-	public Component(){
+
+	public Component() {
 		count++;
 		id = count;
 	}
-	
-	public int getID(){
+
+	public int getID() {
 		return id;
 	}
-	
-	public void update(){
+
+	public void update() {
 		updated = true;
 		updateSubComponent(attachedComponents.size() - 1);
 	}
-	
-	/**	Recursively updates all sub components attached to the component
-	 *  @param sub component element to start updating from 
-	 *  Precondition: -1 <= com < sceneObjects.size()
+
+	/**
+	 * Recursively updates all sub components attached to the component
+	 * 
+	 * @param sub
+	 *            component element to start updating from Precondition: -1 <=
+	 *            com < sceneObjects.size()
 	 */
-	public void updateSubComponent(int com){
-		/* your code here */
+	public void updateSubComponent(int com) {
+		if (com >= 0) {
+			attachedComponents.get(com).update();
+			updateSubComponent(com - 1);
+		}
 	}
-	
-	public boolean isUpdated(){
+
+	public boolean isUpdated() {
 		return updated;
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		return "" + updated;
 	}
 }
